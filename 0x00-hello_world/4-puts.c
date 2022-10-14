e45f61781aa752be9efe9a4bace366bb8349166d
amoslucky <aluckya100@gmail.com>
2022-10-13 19:09:46 -0700

    Add ./4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,11 @@
  main - is the entry point
  always return 0 to mark the end of the program
  */
-int main(void){
+int main(void)
+{
+
 
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 
 }
