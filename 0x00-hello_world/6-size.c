48d9a33e3493b276942a2d5248547cf6a015f4c7
amoslucky <aluckya100@gmail.com>
2022-10-13 19:48:07 -0700

    Add 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char : %lu bytes(s)\n",sizeof(acharacter));
+	printf("Size of a char : %lu bytes(s)\n",sizeof(ainteger));
+	printf("Size of a char : %lu bytes(s)\n",sizeof(along));
+        printf("Size of a char : %lu bytes(s)\n",sizeof(alonglong));
+        printf("Size of a char : %lu bytes(s)\n",sizeof(afloat));
+
+	return (0);
+}
