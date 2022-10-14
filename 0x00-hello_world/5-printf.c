7b3ce72e5bf92160cabadda8e1bfb397b3cd31ed
amoslucky <aluckya100@gmail.com>
2022-10-13 19:21:36 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main is the entrance point of the app
+ * Returns: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
