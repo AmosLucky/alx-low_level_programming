91f9873530ba7715024ea73e91df84973ad2bf03
amoslucky <aluckya100@gmail.com>
2022-10-13 19:01:50 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void){
+/*
+ main - is the entry point
+ always return 0 to mark the end of the program
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
