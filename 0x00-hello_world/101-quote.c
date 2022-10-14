3c5fa13d1174237b23acd86bc0719c7ac6286e89
amoslucky <aluckya100@gmail.com>
2022-10-13 20:40:27 -0700

    Add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 /**
  * main: - Entry point
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Description: 'and that piece of art is useful- Dora Korpar'
  *
  * Return: always Return 0 (Success)
  */
 
-int main(void){
+int main(void)
+{
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
