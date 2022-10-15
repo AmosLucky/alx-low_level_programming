dc4da9e2ad20b32e7c2308369352a364a501eb1d
amoslucky <aluckya100@gmail.com>
2022-10-14 20:59:58 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entrance point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	cha a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
