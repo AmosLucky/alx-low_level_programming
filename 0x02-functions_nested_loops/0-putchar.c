a998d7c6ee5a2d1ba3e9b11b1f34adee6fde13a2
amoslucky <aluckya100@gmail.com>
2022-10-18 20:15:01 -0700

    Fixed error  ./0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
-#include <unistd.h>
+
 /*
- * _putchar - writes the char c tostdout.
- * Return: success 1, error -1
+ * main - prints _putchat.
+ * Return: 0 success 
  *
  */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
