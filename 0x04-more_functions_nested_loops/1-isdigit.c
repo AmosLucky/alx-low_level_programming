65ace0a3c0021233bf0b5c3e79101e3053dd244a
amoslucky <aluckya100@gmail.com>
2022-10-20 21:30:29 -0700

    Soved task 1, 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,21 @@
 #include "main.h"
+
 /**
- * _isdigit is a function used to check for a digit 1 - 9
- * @c: character to be entered.
- * Return: 1 is char c is a digit and 0 if it is not a digit
+ * _isdigit - checks for lowercase character.
+ * @c: caracter recibo por main.
+ * Return: 1 if c is uppercase
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+}	}
 	else
 	{
 		return (0);
