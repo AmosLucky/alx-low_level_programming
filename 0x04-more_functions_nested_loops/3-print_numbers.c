11d86b6b78bf02ca7a57a7c1053238f9bf4e3838
amoslucky <aluckya100@gmail.com>
2022-10-20 21:54:50 -0700

    Soved task 3 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_numbers - prints 0 to 9
- * Return: 0 -9
+ * Return: nothing
  */
 void print_numbers(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
+	chat i;
+	for (i = '0'; i < '10'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
