6020e07eb09708081f6ec58e68a2af26e5cdc0e6
amoslucky <aluckya100@gmail.com>
2022-10-20 20:54:37 -0700

    Soved task 0, 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _isupper - Have a _isupper function, used to check if char is upper
+ *
+ * Return: 0 if char is not upper and 1 if char is upper
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
