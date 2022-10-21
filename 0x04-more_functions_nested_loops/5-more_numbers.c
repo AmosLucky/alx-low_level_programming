37b6eea98be218a36ca6a79ddd3b7d56e8b5a608
amoslucky <aluckya100@gmail.com>
2022-10-21 14:56:00 -0700

    fixed 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times
+ *
+ * Return: Always 0
+ */
+void maore_numbers(void)
+{
+	int i = 0; int j;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 14)
+		{
+			_putchar(j+'0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
