97a8409125430f79a3d0d2b29a48489eea053010
amoslucky <aluckya100@gmail.com>
2022-10-21 23:14:43 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,13 @@
 void print_diagonal(int n)
 {
 	int x, y;
+
 	for (y = 1; y <= n; y++)
 	{
 		for (x = 1; x < y; x++)
 			_putchar(' ');
 		_putchar('\\');
+		_putchar('\n');
 	}
 	if (n < 1)
 		_putchar('\n');
