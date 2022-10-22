709dd2612d17cb35615a8504981aabc4ea5d9209
amoslucky <aluckya100@gmail.com>
2022-10-22 00:23:38 -0700

    Solved print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,23 @@
 #include "main.h"
+
 /**
- * print_triangle - prints a triangle in the terminal
- *
- * @size: Size of the triangle
- *
+ * print_triangle - print a triangle
+ * @size: demensions of triangle
  * Return: nothing
- */
+ **/
+
 void print_triangle(int size)
 {
 	int i, j;
-	if (size >= 1)
+
+	for (i = 1; i <= size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
+		for (j = size - i; j > 0; j--)
+			_putchar(' ');
+		for (j = i; j > 0; j--)
+			_putchar('#');
 		_putchar('\n');
 	}
+	if (size < 1)
+		_putchar('\n');
 }
