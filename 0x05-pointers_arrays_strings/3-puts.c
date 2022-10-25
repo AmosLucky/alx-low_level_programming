6d5eccc31af1893841286e37a254ff4eaf0cad9b
amoslucky <aluckya100@gmail.com>
2022-10-25 13:15:45 -0700

    Solvd task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts print strings in stdout
+ * @str: input char params
+ * Description: prints strings in stdout
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str++);
+	}
+	_putchar('\n');
+}
