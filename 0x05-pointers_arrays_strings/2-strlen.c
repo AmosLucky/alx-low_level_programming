355cde231c24c672cd6762151c6f1e9bc372e4b4
amoslucky <aluckya100@gmail.com>
2022-10-25 12:43:23 -0700

    Fixed task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include  "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the char array parameter
+ * Description: Returns the length of a string
+ * Return: interger
+ */
+int _strlen(char *s)
+{
+	int length = strlen(s);
+	return (length);
+}
