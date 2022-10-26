4de8cc973c91c337e96aaa2c83c6511aaad48eea
amoslucky <aluckya100@gmail.com>
2022-10-25 23:22:43 -0700

    Fixed tsk 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: input char array
+ * @n: numbers of item to be printed
+ * Description: prints n elements of an array of integers
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n-1))
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ",a[i]);
+		}
+	}
+}
