7ac029d09bba09b4ec794ba6b57c8df432ac973c
amoslucky <aluckya100@gmail.com>
2022-10-27 04:39:01 -0700

    fixed task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,19 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
+	int r = 0;
+	int tam = 0;
 
-	while (dest[i] != '\0')
+	for (; dest[tam];)
 	{
-		i++;
+		tam++;
 	}
-	while (src[j] != '\0' && n > j)
+
+	while (r < n && src[r])
 	{
-		dest[i] = src[j];
+		dest[tam + i] = src[r];
 		i++;
-		j++;
+		r++;
 	}
 	return (dest);
 }
