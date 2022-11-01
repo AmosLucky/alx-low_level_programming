cfd2989c56b61100305136ed2a14a0da3f8d1210
amoslucky <aluckya100@gmail.com>
2022-10-31 20:31:38 -0700

    -

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: input char array
+ * @c: char to be located
+ * Description: function that locates a character in a string.
+ * Return: char or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
