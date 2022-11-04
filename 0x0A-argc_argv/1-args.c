a933a93996c40117e2f305499662b452501251ee
amoslucky <aluckya100@gmail.com>
2022-11-04 11:53:45 -0700

    Fixed task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all argument passed
+ * @argc: number of argument passed
+ * @argv: array of arguments passed
+ * Description: The program prints the number of args passed
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
