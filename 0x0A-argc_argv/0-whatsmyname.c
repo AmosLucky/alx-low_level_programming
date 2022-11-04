e11c701b3a9907d4a2cd9e7831f4aa74d76433a9
amoslucky <aluckya100@gmail.com>
2022-11-04 11:31:46 -0700

    Fixed task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print argv[0]
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: nothing
+ */
+int main(int argc, char* argv)
+{
+	printf(argv[0]);
+}
