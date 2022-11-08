055dd358ab7f64c15b7dfbb68bfe99bbcea4a391
amoslucky <aluckya100@gmail.com>
2022-11-08 14:56:39 -0800

    Add _putchar.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,7 @@
 char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+int _putchar(char c);
+char **strtow(char *str);
