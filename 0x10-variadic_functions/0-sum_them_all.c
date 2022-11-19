d52cd2b9140e0cc1aa0f188628d36459c2333145
amoslucky <aluckya100@gmail.com>
2022-11-19 12:26:27 -0800

    Fixed task 0 again

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-    {
-        return (0);
-    }
-    else
-    {
-        return (n);
-    }
-    
+	if (n == 0)
+		return (0);
+	int i;
+	int sum = 0;
+	va_list numbers;
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
 }
